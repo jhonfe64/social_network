c8b82a184e05c59ab07feb0e872cde20f75f0e34
john <jhonfe649@gmail.com>
2023-12-06 18:05:03 -0500

    publications

--- controllers/publication.js
+++ controllers/publication.js
@@ -1,128 +1,178 @@
-const Publication = require("../models/publication")
-
+const Publication = require("../models/publication");
+const followService = require("../services/followService");
 
 //acciones de prueba
 const pruebaPublication = (req, res) => {
-    return res.status(200).send({
-        message: "MENSAGE ENVIDADO DESDE EL CONTROLADOR publication"
-    })
-}
-
-
-
+  return res.status(200).send({
+    message: "MENSAGE ENVIDADO DESDE EL CONTROLADOR publication",
+  });
+};
 
 //guardar publicacion
 const save = async (req, res) => {
+  const params = req.body;
+  console.log("estos son los params", params);
 
-    const params = req.body
-    console.log("estos son los params", params)
+  if (!params.text) {
+    return res.status(400).json({
+      status: "error",
+      message: "Debes enviar el texto de la publicacion",
+    });
+  }
 
-    if(!params.text) {
-        return res.status(400).json({
-            status: "error",
-            message: "Debes enviar el texto de la publicacion"
-        })
+  //Objetodo del modelo
+  const newPublication = new Publication(params);
+  newPublication.user = req.user.id;
+
+  //guaradar el objeto
+  try {
+    const savedPublication = await newPublication.save();
+
+    if (!savedPublication) {
+      return res.status(500).json({
+        status: "error",
+        message: "No se ha podido guardar la publicacion",
+      });
     }
 
-    //Objetodo del modelo
-    const newPublication = new Publication(params)
-    newPublication.user = req.user.id
-
-    //guaradar el objeto
-    try {
-        const savedPublication = await newPublication.save()
-
-        if(!savedPublication){
-            return res.status(500).json({
-                status: "error",
-                message: "No se ha podido guardar la publicacion"
-            })
-        }
-
-        return res.status(200).json({
-            status: "success",
-            message: "Se ha guardado la publicacion",
-            savedPublication
-        })
-    } catch (error) {
-        return res.status(500).json({
-            status: "error",
-            message: "No se ha podido guardar la publicacion"
-        })
-    }
-}
+    return res.status(200).json({
+      status: "success",
+      message: "Se ha guardado la publicacion",
+      savedPublication,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: "error",
+      message: "No se ha podido guardar la publicacion",
+    });
+  }
+};
 
 //mostrar una sola publicacion
 const detail = async (req, res) => {
-    //sacar el id
-    const publicationId = req.params.id
+  //sacar el id
+  const publicationId = req.params.id;
 
-    //hacer el find para obtener la publicacion
-    try {
-        const publication = await Publication.findById(publicationId)
-        if(!publication) return res.status(400).json({status: "error", message: "No existe esta publicacion"})
-        return res.status(200).json({
-            status: "success",
-            publication
-        })
-    } catch (error) {
-        return res.status(500).json({
-            status: "error",
-            message: "No se puede obtener la publicacion",
-        })
-    }
-
-
-
- 
-}
+  //hacer el find para obtener la publicacion
+  try {
+    const publication = await Publication.findById(publicationId);
+    if (!publication)
+      return res
+        .status(400)
+        .json({ status: "error", message: "No existe esta publicacion" });
+    return res.status(200).json({
+      status: "success",
+      publication,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: "error",
+      message: "No se puede obtener la publicacion",
+    });
+  }
+};
 
 //eliminar publicaciones
 const remove = async (req, res) => {
-
-    const {id} = req.params
-    try {
-        const removed = await Publication.findByIdAndDelete(id)
-        if(!removed) {
-            return res.status(404).json({
-                status: "error",
-                message: "esta publicacion no existe"
-            })
-        }
-        return res.status(200).json({
-            status: "success",
-            message: "se esta borrando una publicacion"
-        })
-    } catch (error) {
-        return res.status(500).json({
-            status: "error",
-            message: "No se ha podido borrar la publicacion"
-        })
+  const { id } = req.params;
+  try {
+    const removed = await Publication.findByIdAndDelete({
+      _id: id,
+      user: req.user.id,
+    });
+    if (!removed) {
+      return res.status(404).json({
+        status: "error",
+        message: "esta publicacion no existe",
+      });
     }
-}
+    return res.status(200).json({
+      status: "success",
+      message: "se esta borrando una publicacion",
+      publicationId: id,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: "error",
+      message: "No se ha podido borrar la publicacion",
+    });
+  }
+};
 
-//listar todas las publicaciones de los usuarios que sigo
+//listar todas las publicaciones del usuario por id + populate
+const user = async (req, res) => {
+  const { id: userId } = req.params;
 
+  //populamos para traer las publicaciones con el usaurio
+  try {
+    const userPublications = await Publication.find({ user: userId })
+      .sort("-created_at")
+      .populate("user", "-password")
+      .exec();
 
-//listar publicaciones de un usuario en concreto
+    if (!userPublications) {
+      return res.status(404).json({
+        status: "error",
+        message: "No se encontraron publicaciones",
+      });
+    }
 
+    return res.status(200).json({
+      status: "success",
+      message: "ESTA ES LA LISTA DE PUBLICACIONES DE UN USUARIO",
+      user: req.user,
+      userId,
+      userPublications,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: "error",
+      message: "No se cargaron publicaciones",
+    });
+  }
+};
+
+//listar publicaciones de los usarios que yoe stoy siguiendo
+const feed = async (req, res) => {
+  //sacar el array de ids de los usuarios que yo sigo como usuario logueado
+  try {
+    const myFollows = await followService.followUserIds(req.user.id);
+
+    // const publications = await Publication.find({
+    //   user: myFollows.following,
+    // }).exec();
+
+    const publications = await Publication.find({user: { $in: myFollows.following }})
+      .populate("user")
+      .sort("-created_at");
+
+    //buscar las publicaciones correspondientes a esos ids
+
+    return res.status(200).json({
+      status: "success",
+      message: "estos son los ids de los usuarios que sigo",
+      myFollows: myFollows.following,
+      publications,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: "error",
+      message: "No se pudieron obtener los seguidores",
+    });
+  }
+};
 
 //subir fficheros
 
-
-//devolver imagenes 
-
-
-
-
-
-
+//devolver imagenes
 
 //EXPORTAR ACCIONES
 
 module.exports = {
-    pruebaPublication,
-    save,
-    detail,
-    remove
-}
+  pruebaPublication,
+  save,
+  detail,
+  remove,
+  user,
+  feed,
+};
--- routes/publication.js
+++ routes/publication.js
@@ -1,14 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {pruebaPublication, save, detail, remove} = require('../controllers/publication')
-const {auth} = require("../middlewares/auth")
+const {
+  pruebaPublication,
+  save,
+  detail,
+  remove,
+  user,
+  feed
+} = require("../controllers/publication");
+const { auth } = require("../middlewares/auth");
 
 //definir las rutas
 
-router.get("/prueba-publication", pruebaPublication)
-router.post("/save",auth,  save)
-router.get("/detail/:id", auth, detail)
-router.delete("/remove/:id", auth, remove)
+router.get("/prueba-publication", pruebaPublication);
+router.post("/save", auth, save);
+router.get("/detail/:id", auth, detail);
+router.delete("/remove/:id", auth, remove);
+router.get("/user/:id", auth, user);
+router.get("/feed", auth, feed);
 
 //exportar router
-module.exports = router
+module.exports = router;
